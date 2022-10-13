189b1728b57cb241c872fedb27b48799b067e359
formula38 <louismconley@gmail.com>
2022-10-13 14:58:39 -0700

    Configuring employee.component.ts

--- angular_frontend/employeemanagerapp/src/app/components/employee/employee.component.ts
+++ angular_frontend/employeemanagerapp/src/app/components/employee/employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from "../../services/employeeservices/employee.service";
+import {Employee} from "../../employee";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+  constructor(private employeeService: EmployeeService) { }
 
-  constructor() { }
+  public employees: Employee[] | undefined;
 
   ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
 }
