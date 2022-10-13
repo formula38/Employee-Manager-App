8febc2e3acb18a43ec4b160331f499f0226c1be0
formula38 <louismconley@gmail.com>
2022-10-13 09:58:26 -0700

    initial commit

---
+++ src/main/java/com/cloud38/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package com.cloud38.employeemanager.repo;
+
+import com.cloud38.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(long id);
+
+    Optional findEmployeeById(Long id);
+}
